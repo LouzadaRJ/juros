427abffc3cb93d6c23f3e385b96c382d64e3ab0d
LouzadaRJ <matheuslouzadaa@gmail.com>
2023-10-31 16:06:42 -0300

    simple calc

--- juros.js
+++ juros.js
@@ -1,9 +1,15 @@
 import entradaDados from 'readline-sync'
 
-console.log("Aplicação de cálculo de juros\n")
+console.log("Aplicação de cálculo de juros\n");
 
-let valor = entradaDados.question("Informe o valor devido: R$ ")
-let dias = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ")
+let valor_divida = entradaDados.question("Informe o valor devido: R$ ");
+let dias_vencimento = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ");
 
-console.log("Valor original da dívida: R$ "+valor)
-console.log("Dias atrasados: "+dias)
+let juros = 10;
+let valor_juros = (valor_divida / 100) * juros;
+let total_divida = Number(valor_divida) + Number(valor_juros);
+
+console.log("Valor original da dívida: R$ "+valor_divida);
+console.log("Dias atrasados: "+dias_vencimento);
+console.log("Taxa de juros: "+juros+"%");
+console.log("Valor total com juros: R$ "+total_divida);
