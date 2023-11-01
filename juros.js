d2779ff5e7736c1c564523d8d81cf6d1b0a93f93
LouzadaRJ <matheuslouzadaa@gmail.com>
2023-11-01 09:50:24 -0300

    complit aplication

--- juros.js
+++ juros.js
@@ -1,23 +1,38 @@
 import entradaDados from 'readline-sync'
 
-console.log("Aplicação de cálculo de juros\n");
+console.log("\nAplicação de cálculo de juros\n");
+
+let valor_juros;
 
 let valor_divida = entradaDados.question("Informe o valor devido: R$ ");
-let dias_vencimento = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ");
 
+if(valor_divida > 0){ 
+    let dias_vencimento = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ");
 
-if(valor_divida <= 0){
-    console.log("O valor da dívida deve ser maior que zero!")
+    if(dias_vencimento > 0){
+     
+        if(dias_vencimento > 15){
+            valor_juros = 10;
+        }
+        else{
+            valor_juros = 5
+        }
+
+        valor_divida = Number(valor_divida);
+        dias_vencimento = Number(dias_vencimento);
+
+        let total_juros = (valor_divida / 100) * valor_juros;
+        let valor_total = Number(valor_divida) +  Number(total_juros);
+
+        console.log("\nValor original da dívida: R$ "+valor_divida);
+        console.log("Dias atrasados: "+dias_vencimento);
+        console.log("Taxa de Juros: "+valor_juros+"%");
+        console.log("Valor total com juros: R$ "+valor_total+"\n");
+    }
+    else{
+        console.log("Você está em dia!");
+    }
 }
-else(valor_divida > 0){
-
-    let juros = 10;
-    
-    let valor_juros = (valor_divida / 100) * juros;
-    let total_divida = Number(valor_divida) + Number(valor_juros);
-    
-    console.log("Valor original da dívida: R$ "+valor_divida);
-    console.log("Dias atrasados: "+dias_vencimento);
-    console.log("Taxa de juros: "+juros+"%");
-    console.log("Valor total com juros: R$ "+total_divida);
+else{
+    console.log("Valor da dívida deve ser maior que zero!")
 }
